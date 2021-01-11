4ebb941dcb04154f5192d524860877a2682bdf62
Eric <eric.r.fern@gmail.com>
2021-01-10 21:16:58 -0600

    change it another time

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My Portfolio</h1>
+        <h1>My demo site</h1>
       </header>
+
+      <main className="App-main">
+        <p>Welcome to my demo site!</p>
+      </main>
     </div>
   );
 }
 
 export default App;
-
